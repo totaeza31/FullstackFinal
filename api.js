af8248f122c21adfa3d272950c2b4664c3b503bd
totaeza31 <47884416+totaeza31@users.noreply.github.com>
2019-12-03 17:38:07 +0700

    5

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const Product = require('./models/productSchema')
 // #5 Change URL to your local mongodb
-const url = "";
+const url = "mongodb://localhost:27017/coc";
 // ===============================
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
